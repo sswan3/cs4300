1c47354dfe9ca5c50e92815833163100a1bdad09
sswan3 <savannahhswan@gmail.com>
2026-09-25 03:37:33 +0000

    task7 modified again

--- homework1/src/task7.py
+++ homework1/src/task7.py
@@ -1,18 +1,14 @@
 import numpy as np
 
 def average_numpy(numbers):
-    """function that demonstrates the use of numpy 
-    by converting a list of numbers to a numpy array and finding the average"""
-
-    numpy_array = np.array(numbers)
-    sum = 0
-    count = 0
-    for i in numpy_array:
-        sum += i
-        count += 1
-    average = sum/count
-    print(average)
+    """function that demonstrates the use of numpy by finding the average"""
+    average = np.mean(numbers)
     return average
 
+def min(numbers):
+    smallest_value = np.min(numbers)
+    return smallest_value
+
 numbers = [4,2,1,6,34,]
-average_numpy(numbers)
+print(average_numpy(numbers))
+print(min(numbers))
--- homework1/src/test_task7.py
+++ homework1/src/test_task7.py
@@ -1,7 +1,28 @@
 from task7 import average_numpy
+from task7 import min
 import numpy as np
 
 def test_average_numpy():
-    numbers = [4,2,1,6,34,]
+    numbers = [4,2,1,6,34]
     average = average_numpy(numbers)
-    assert average == 9.4
+    sum = 0
+    count = 0
+    for i in numbers:
+        sum += i
+        count += 1
+    test_average = sum/count
+    assert average == test_average
+
+def test_min():
+    numbers = [4,2,1,6,34]
+    numpy_min = min(numbers)
+
+    min_test = numbers[0]
+    for i in numbers:
+        if i < min_test:
+            min_test = i
+    assert numpy_min == min_test
+
+
+
+
