f84b70d82eed1cd7d919ade14f3e056ecdbb8490
sswan3 <savannahhswan@gmail.com>
2026-09-25 04:36:03 +0000

    test6 file does not exist edits

--- homework1/src/task6.py
+++ homework1/src/task6.py
@@ -1,27 +1,30 @@
 import string
 from pathlib import Path
-def word_count():
+def word_count(file_name):
     """function that reads from a file and counts the words and returns the word count"""
     BASE_DIR = Path(__file__).resolve().parent.parent
-    FILE_PATH = BASE_DIR / "task6_read_me.txt"
+    FILE_PATH = BASE_DIR / file_name
 
     count = 0
     list = []
     list2 = []
 
-    with open(FILE_PATH, "r", encoding="utf-8") as file:
-        for line in file:
-            # Split each line into individual words
-            for word in line.split():
-                list.append(word)
+    if not FILE_PATH.exists():
+        raise FileNotFoundError("file is missing")
+    else:
+        with open(FILE_PATH, "r", encoding="utf-8") as file:
+            for line in file:
+                # Split each line into individual words
+                for word in line.split():
+                    list.append(word)
 
-    for i in list:
-        # .strip() strips any hidden newline or carriage return characters first
-        clean = i.strip().translate(str.maketrans("", "", string.punctuation))
-        if clean:  # skips adding empty strings if a token was strictly punctuation (like "." or ",")
-            list2.append(clean)
+        for i in list:
+            # .strip() strips any hidden newline or carriage return characters first
+            clean = i.strip().translate(str.maketrans("", "", string.punctuation))
+            if clean:  # skips adding empty strings if a token was strictly punctuation (like "." or ",")
+                list2.append(clean)
 
 
-    word_count = len(list2)
-    print(word_count)
-    return word_count
+        word_count = len(list2)
+        print(word_count)
+        return word_count
--- homework1/test/test_task6.py
+++ homework1/test/test_task6.py
@@ -1,6 +1,15 @@
 from task6 import word_count
+from pathlib import Path
+import pytest
 
-#verify word count is correct
+
 def test_word_count_accuracy():
-    count = word_count()
+    """verify word count is correct and function works for file that exists"""
+    count = word_count("task6_read_me.txt")
     assert count == 104
+
+def test_word_count_file_not_exist():
+    """handling for a file that does not exist"""
+    with pytest.raises(FileNotFoundError):
+        count = word_count("task5_read_me.txt")
+
